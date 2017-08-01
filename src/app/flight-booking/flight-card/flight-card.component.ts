cdb21d9b97f9f1e7d6d5d0830c4c04bf2b1f205c
ManfredSteyer <manfred.steyer@gmx.net>
2017-08-01 11:10:48 +0200

    components

---
+++ src/app/flight-booking/flight-card/flight-card.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Flight } from '../../entities/flight';
+
+@Component({
+  selector: 'flight-card',
+  templateUrl: './flight-card.component.html',
+  styleUrls: ['./flight-card.component.css']
+})
+export class FlightCardComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() item: Flight;
+  @Input() selected: boolean;
+  @Output() selectedChange = new EventEmitter<boolean>();
+
+  ngOnInit() {
+  }
+
+  select() {
+    this.selected = true;
+    this.selectedChange.next(this.selected);
+  }
+
+  deselect() {
+    this.selected = false;
+    this.selectedChange.next(this.selected);
+  }
+
+}
